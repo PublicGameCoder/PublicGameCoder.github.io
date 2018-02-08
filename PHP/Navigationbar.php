7251af252b52c433cb5953beca24f93da90236e4
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-02-08 10:01:13 +0100

    Upload Backup code

---
+++ PHP/Navigationbar.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<div class="menubox">
+	<a class="menuOption" href="index.php">Home</a>
+	<a class="menuOption" href="portfolio.php">Portfolio</a>
+	<a class="menuOption" href="profile.php">Profile</a>
+	<a class="menuOption" href="contact.php">Contact</a>
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
+		<title>Home</title>
+	</head>
+		<style>
+			<?php include 'CSS/mainStyle.css'; ?>
+		</style>
+	<body>
+	<?php
+		include "PHP/Navigationbar.php";
+	?>
+    </body>
+</html>
